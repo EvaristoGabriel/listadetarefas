096349f69018225bd0754ca877051133a131d8f3
EvaristoGabriel <gabriel.evaristo@estudante.ufjf.br>
2022-02-17 20:42:54 -0300

    adicionando botoes para mover para cima e para baixo, botao de apagar tudo e botao para editar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let botao = document.getElementById("botaoadd")
 let tarefa = document.getElementById("nome-tarefa-id")
 let lista = document.getElementById("tarefas")
 let arraydetarefa = []
+let botaolimpar = document.getElementById("limpar")
 
 recuperartarefa()
 
@@ -12,6 +13,9 @@ function mostrartarefa() {
         novali = novali + `<li class="listas ${tarefa.status == true ? "concluido" : ""}">
         <button class="botao-tick" onclick="concluir(${index})"><img src="/img/Tick.png" alt="Tick" class="imagem"></button>
         <p class="nome-tarefa ${tarefa.status == true ? "concluido" : ""}" id="nome-tarefa-id">${tarefa.tarefa}</p>
+        <button class="botao-editar" ondblclick="editar(${index})"></button>
+        <button class="botao-up" onclick="mudarcima(${index})"><img src="/img/setaup.png" alt="Mover cima" class="imagem"></button>
+        <button class="botao-down" onclick="mudarbaixo(${index})"><img src="/img/setadown.png" alt="Mover baixo" class="imagem"></button>
         <button class="botao-delete" onclick="deletar(${index})"><img src="/img/Lixeira.png" alt="Lixeira" class="imagem"></button>
     </li>`
     })
@@ -44,6 +48,33 @@ function concluir(index) {
     mostrartarefa()
 }
 
+function editar(index){
+    let entrada = prompt("Editar", arraydetarefa[index].tarefa)
+    arraydetarefa[index].tarefa = entrada
+
+    mostrartarefa()
+}
+
+function mudarcima(index){
+    if(index > 0){
+        let aux = arraydetarefa[index]
+        arraydetarefa[index] = arraydetarefa[index-1]
+        arraydetarefa[index-1] = aux
+    }
+    
+    mostrartarefa()
+}
+
+function mudarbaixo(index){
+    if(index < arraydetarefa.length-1){
+        let aux = arraydetarefa[index]
+        arraydetarefa[index] = arraydetarefa[index+1]
+        arraydetarefa[index+1] = aux  
+    }
+    
+    mostrartarefa()
+}
+
 function recuperartarefa() {
     let minhatarefa = localStorage.getItem("Lista")
     if(minhatarefa){
@@ -58,11 +89,16 @@ function addenter(teclas){
     }
 }
 
+function limpar(){
+    arraydetarefa.length = 0
+    mostrartarefa()
+}
+
 botao.addEventListener("click", addtarefa)
 
 document.addEventListener("keypress",addenter)
 
-
+botaolimpar.addEventListener("click", limpar)
 
 
 
